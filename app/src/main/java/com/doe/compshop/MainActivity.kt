e224eb2ec40ca6c0853b878cb48d180345fa4c87
VictorKabata <39780120+VictorKabata@users.noreply.github.com>
2021-04-21 23:06:49 +0300

    Added app icons

--- app/src/main/java/com/doe/compshop/MainActivity.kt
+++ app/src/main/java/com/doe/compshop/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
